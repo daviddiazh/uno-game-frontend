6df23a809239de441588b14eaed712d204e2a533
daviddiazh <daviddiazlds1221@gmail.com>
2024-07-17 17:44:31 -0500

    feat: enrollment page contains theirs styles

--- src/pages/Enrollment/index.tsx
+++ src/pages/Enrollment/index.tsx
@@ -1,5 +1,80 @@
+import type { FormProps } from 'antd';
+import { Button, Form, Input } from 'antd';
+import styles from './styles.module.css';
+import { avatars } from '../../utils/avatars';
+
+type FieldType = {
+  email?: string;
+  password?: string;
+  name?: string;
+};
+
 export const Enrollment = () => {
+
+  const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+    console.log('Success:', values);
+  };
+  
+  const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div>Enrollment</div>
+    <div className={styles['container-page']}>
+      <div className={styles.content}>
+        <h1>Registro!</h1>
+        <div style={{width: '100%'}}>
+          <Form
+            name="basic"
+            style={{ maxWidth: 'none' }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            layout="vertical"
+          >
+            <Form.Item<FieldType>
+              label="Nombre"
+              name="name"
+              rules={[{ required: true, message: 'El nombre es obligatorio' }]}
+            >
+              <Input placeholder="Nombre completo" style={{maxWidth: 450}} />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              label="Correo"
+              name="email"
+              rules={[{ required: true, message: 'Please input your email!' }]}
+            >
+              <Input placeholder="Correo electrónico" style={{maxWidth: 450}} />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              label="Contraseña"
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password placeholder="Contraseña" style={{maxWidth: 450}} />
+            </Form.Item>
+
+            <p>Escoge tu avatar</p>
+            <div className={styles['container-avatar']}>
+              {
+                avatars.map(item => (
+                  <div key={item.id} onClick={() => console.log({item})}>
+                    <img src={item.url} alt="Avatar" className={styles.avatar} />
+                  </div>
+                ))
+              }
+            </div>
+
+            <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
+              <Button type="primary" htmlType="submit" className={styles.btn}>
+                Crear cuenta
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    </div>
   )
 }
---
+++ src/utils/avatars.ts
@@ -0,0 +1,26 @@
+export const avatars = [
+    {
+      id: 0,
+      url: 'https://lh3.googleusercontent.com/d/10JFjSTDMUCBzyodgvqIq3H1PrFlncIEN?authuser=0',
+    },
+    {
+      id: 1,
+      url: 'https://lh3.googleusercontent.com/d/16HHERmXd_JZtLaExlkku9_D3OurJJjVV?authuser=0',
+    },
+    {
+      id: 2,
+      url: 'https://lh3.googleusercontent.com/d/17vIUK0ffWgNeP9JVRtdaQPw-YGgLKOTS?authuser=0',
+    },
+    {
+      id: 3,
+      url: 'https://lh3.googleusercontent.com/d/1pS4wSXSl7YtBLtkU28kdySQZoayrDoHT?authuser=0',
+    },
+    {
+      id: 4,
+      url: 'https://lh3.googleusercontent.com/d/1hgS2MXt444v7v5YkULTyRzUPtRg7-sf-?authuser=0',
+    },
+    {
+      id: 5,
+      url: 'https://lh3.googleusercontent.com/d/1ifEopDLU27LISEsNovHwJLDPpaiJ5iu8?authuser=0',
+    },
+];
